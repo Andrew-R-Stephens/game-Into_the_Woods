3908cc6cf3d68b32cc8f88c408dce72c9f6f48d1
Andrew Stephens <asteph11@oldwestbury.edu>
2022-04-25 13:21:33 -0400

    Javadoc (excludes) - AMenuEnvironment

--- SourceCode/Project/src/main/java/models/prototypes/environments/menu/AMenuEnvironment.java
+++ SourceCode/Project/src/main/java/models/prototypes/environments/menu/AMenuEnvironment.java
@@ -10,32 +10,35 @@ import java.awt.*;
 import java.util.Stack;
 
 /**
- * <p>AMenuEnvironment</p>
+ * <p>AMenuEnvironment class is an AEnvironment subtype that controls any Menu Environment and its contained
+ * entities.</p>
+ * <p>Child subtypes include both MainMenuEnvironment and PauseMenuEnvironment.</p>
  */
 public abstract class AMenuEnvironment extends AEnvironment implements IDrawable, IUpdatable {
 
     private final Stack<AMenu> menuStack = new Stack<>();
 
     /**
-     * <p></p>
-     * @param parentEnvironmentsModel -
-     * @param controlsModel -
+     * <p>Initializes the AMenuEnvironment through super calls from the subclass.</p>
+     * @param parentEnvironmentsModel The containing EnvironmentsHandler
+     * @param controlsModel The ControlsModel that this Environment should be using.
      */
     public void init(EnvironmentsHandler parentEnvironmentsModel, MenuControls controlsModel) {
         super.init(parentEnvironmentsModel, controlsModel);
     }
 
     /**
-     * <p></p>
-     * @param defaultMenu -
+     * <p>Initializes the first page with a default AMenu.</p>
+     * @param defaultMenu The chosen AMenu subtype to be added. Should not be null.
      */
     public void initPage(AMenu defaultMenu) {
         push(defaultMenu);
     }
 
     /**
-     * <p></p>
-     * @param menu -
+     * <p>Accepts an AMenu subtype and pushes it to the top of a stack of AMenus. The top Menu will always be the
+     * one that is active.</p>
+     * @param menu The AMenu that should be added to the top of the AMenu stack.
      */
     public void push(AMenu menu){
         if(!menuStack.isEmpty()) {
@@ -46,16 +49,16 @@ public abstract class AMenuEnvironment extends AEnvironment implements IDrawable
     }
 
     /**
-     * <p></p>
-     * @return
+     * <p>Returns the reference to the top-most Menu in the stack.</p>
+     * @return The top-most Menu.
      */
     public AMenu peek() {
         return menuStack.peek();
     }
 
     /**
-     * <p></p>
-     * @return
+     * <p>Removes the top-most Menu in the stack and returns it.</p>
+     * @return the top-most menu in the stack. Null if the size of the stack is <= 1.
      */
     public AMenu pop() {
         if(menuStack.size() > 1)
@@ -65,8 +68,8 @@ public abstract class AMenuEnvironment extends AEnvironment implements IDrawable
     }
 
     /**
-     * <p></p>
-     * @return
+     * <p>Returns the size of the stack</p>
+     * @return the quantity of menus in the stack.
      */
     protected int getStackDepth() {
         return menuStack.size();
