faf5480f9e7f562e765c1a40c361a0bcea0fee5a
Andrew Stephens <asteph11@oldwestbury.edu>
2022-04-25 12:32:36 -0400

    Javadoc (excludes) - GameEnvironment

--- SourceCode/Project/src/main/java/models/environments/game/GameEnvironment.java
+++ SourceCode/Project/src/main/java/models/environments/game/GameEnvironment.java
@@ -29,22 +29,19 @@ import java.util.Queue;
 import java.util.Random;
 
 /**
- * <p></p>
+ * <p>GameEnvironment class is an AEnvironment subtype that controls the Game Environment and its contained entities.</p>
+ * <p>Controls the PauseMenuEnvironment when the GameEnvironment is paused.</p>
+ * <p>Contains the GameControls, Levels, Game HUD, Player Inventory, and all Actors.</p>
  */
 public class GameEnvironment extends AEnvironment implements IDrawable, IUpdatable {
 
-    private GameControls gameControls;
-
-    private PauseMenuEnvironment pauseMenuEnvironment;
-
-    private LevelsList levelsList;
-
-    private HUDModel hudModel;
-    private PlayerInventory inventory;
-
     private final ArrayList<AActor> actors = new ArrayList<>();
     private final Queue<AActor> actorsQueue = new LinkedList<>();
-
+    private GameControls gameControls;
+    private PauseMenuEnvironment pauseMenuEnvironment;
+    private LevelsList levelsList;
+    private HUDModel hudModel;
+    private PlayerInventory inventory;
     private PlayerAvatar character;
 
     private boolean isPaused = false;
@@ -52,13 +49,13 @@ public class GameEnvironment extends AEnvironment implements IDrawable, IUpdatab
     private Robot robot;
 
     /**
-     * <p></p>
-     * @param parentEnvironmentsHandler -
-     * @param pauseMenuEnvironment -
-     * @param gameControls -
-     * @param levelsList -
-     * @param hudModel -
-     * @param inventory -
+     * <p>Initializes the GameEnvironment with references to preconfigured object.</p>
+     * @param parentEnvironmentsHandler The encapsulating EnvironmentsHandler. Cannot be null.
+     * @param pauseMenuEnvironment The PauseMenuEnvironment. Cannot be null.
+     * @param gameControls The GameControls. Cannot be null.
+     * @param levelsList The LevelsList. Cannot be null.
+     * @param hudModel The HUDModel. Cannot be null.
+     * @param inventory The PlayerInventory. Cannot be null.
      */
     public void init(EnvironmentsHandler parentEnvironmentsHandler,
                      PauseMenuEnvironment pauseMenuEnvironment, GameControls gameControls,
@@ -83,24 +80,16 @@ public class GameEnvironment extends AEnvironment implements IDrawable, IUpdatab
     }
 
     /**
-     * <p></p>
-     * @param inventory -
-     */
-    private void setPlayerInventory(PlayerInventory inventory) {
-        this.inventory = inventory;
-    }
-
-    /**
-     * <p></p>
-     * @param hudModel -
+     * <p>Sets the preconfigured HUD for the Game Environment.</p>
+     * @param hudModel The HUD Model for the Overlays. Should not be null.
      */
     private void setHUDModel(HUDModel hudModel) {
         this.hudModel = hudModel;
     }
 
     /**
-     * <p></p>
-     * @param pauseMenuEnvironment -
+     * <p>Sets the PauseMenuEnvironment for the paused state of the Game Environment.</p>
+     * @param pauseMenuEnvironment The PauseMenuEnvironment used for the paused Game state. Cannot be null.
      */
     private void setPauseMenuEnvironment(PauseMenuEnvironment pauseMenuEnvironment) {
         this.pauseMenuEnvironment = pauseMenuEnvironment;
@@ -108,10 +97,265 @@ public class GameEnvironment extends AEnvironment implements IDrawable, IUpdatab
 
     /**
      * <p></p>
-     * @param controlsModel -
+     * @param levelsList -
+     */
+    public void setLevelsList(LevelsList levelsList) {
+        this.levelsList = levelsList;
+    }
+
+    /**
+     * <p></p>
+     * @param controlsViewModel -
+     * @param levelModel -
+     */
+
+    private void setPlayerAvatar(GameControls controlsViewModel, LevelsList levelModel) {
+        int[] startPos = levelModel.getCurrentLevel().getCharacterOrigin();
+        // Add in the Main Test Character
+
+        character = new PlayerAvatar (
+                getResources(),
+                controlsViewModel,
+                startPos[0],
+                startPos[1],
+                55, 70,
+                0, 0,
+                true
+        );
+
+    }
+
+    /**
+     * <p></p>
+     * @param levelIndex -
+     */
+    public void setCurrentLevel(int levelIndex) {
+        levelsList.setCurrentLevel(levelIndex);
+    }
+
+    /**
+     * <p>Sets a preconfigured player inventory into the Game Environment.</p>
+     * @param inventory The inventory of the player. Should not be null.
+     */
+    private void setPlayerInventory(PlayerInventory inventory) {
+        this.inventory = inventory;
+    }
+
+    /**
+     * <p></p>
+     * @param paused -
+     */
+    public void setPaused(boolean paused) {
+        isPaused = paused;
+    }
+
+    /**
+     * <p>Builds the environment with a new Player Avatar and pairs the controls and Levels List  directly with the
+     * Player.</p>
+     * @param controlsModel The controls model that should control the PlayerAvatar.
      */
     public void build(GameControls controlsModel) {
         setPlayerAvatar(controlsModel, levelsList);
+    }
+
+    /**
+     * <p>Updates the objects within the Game state. Controls the game objects, does collisions, updates positions, and
+     * updates the Overlay.</p>
+     * @param delta The ratio of actual/target update rate for the game ticks.
+     */
+    public void doGameUpdates(float delta) {
+
+        doGameControls();
+        insertQueuedActors(); // Dequeue queued actors and add them to list of actors
+        detectCollisions(delta); // Check Game Object Collisions with Level Props
+        updateActors(delta); // Update the Game Objects
+        updateLevel(delta); // Update level models.props
+        updateHUD(delta); // Update HUD overlay
+    }
+
+    /**
+     * <p>Controls the pause state of the Environment. Switches to when game is paused.
+     * Allows for use and visuals of pause menu controls.</p>
+     * @param delta The ratio of actual/target update rate for the game ticks.
+     */
+    public void doPauseMenuUpdates(float delta) {
+        doPauseMenuControls();
+
+        pauseMenuEnvironment.update(delta);
+    }
+
+    /**
+     * <p>Listens to Game Controller and produces the requested actions from Key inputs for the game state.</p>
+     */
+    private void doGameControls() {
+        robot.mouseMove(
+                (int) (Toolkit.getDefaultToolkit().getScreenSize().width * .5f),
+                (int) (Toolkit.getDefaultToolkit().getScreenSize().height * .5f)
+        );
+
+        if(getKeyController() instanceof GameKeyControls kc) {
+            if(kc.getControlsModel().getAction(GameControls.Actions.ESCAPE)) {
+                kc.getControlsModel().reset();
+                setPaused(true);
+                getParentEnvironmentsHandler().swapToEnvironment(
+                        EnvironmentsHandler.EnvironmentType.GAME_PAUSE_MENU, false).applyEnvironment();
+            }
+        }
+    }
+
+    /**
+     * <p>Listens to Menu Controller and produces the requested actions from Key inputs for the pause state.</p>
+     */
+    public void doPauseMenuControls() {
+        if(pauseMenuEnvironment.getKeyController() instanceof MenuKeyControls kc) {
+            if(kc.getControlsModel().getAction(MenuControls.Actions.ESCAPE)) {
+                kc.getControlsModel().reset();
+                setPaused(false);
+                pauseMenuEnvironment.onExit();
+                getParentEnvironmentsHandler().swapToEnvironment(
+                        EnvironmentsHandler.EnvironmentType.GAME, false).applyEnvironment();
+            }
+        }
+    }
+
+    /**
+     * <p>Gets the LevelsList object which contains all loaded Levels.</p>
+     * @return The list of all created levels.
+     */
+    public LevelsList getLevelsList() {
+        return levelsList;
+    }
+
+    /**
+     * <p>Updates components within the in-game HUD overlay.</p>
+     * @param delta The ratio of actual/target update rate for the game ticks.
+     */
+    private void updateHUD(float delta) {
+        hudModel.update(delta);
+    }
+
+    /**
+     * <p>Updates components the currently active level.</p>
+     * @param delta The ratio of actual/target update rate for the game ticks.
+     */
+    private void updateLevel(float delta) {
+        levelsList.getCurrentLevel().update(delta);
+    }
+
+    /**
+     * <p>Adds test actors into the level. This is solely used for performance and behavior testing.</p>
+     * @param delta The ratio of actual/target update rate for the game ticks.
+     */
+    private synchronized void testAddingActors(float delta) {
+        if (getMouseController() instanceof GameMouseControls gmc) {
+
+            if (gmc.isLeftPressed()) {
+                int count = (int) (10 / delta);
+                if (count < 1) {
+                    count = 1;
+                }
+                for (int i = 0; i < count; i++) {
+                    queueActor(
+                            new Particle(
+                                    getResources(),
+                                    (-Camera.camX / Config.scaledW_zoom) + (gmc.getPos()[0]/ Config.scaledW_zoom),
+                                    (-Camera.camY / Config.scaledW_zoom) + (gmc.getPos()[1]/ Config.scaledH_zoom),
+                                    10f,
+                                    10f,
+                                    new Random().nextFloat(-5, 5),
+                                    new Random().nextFloat(-5, 5),
+                                    true
+                            )
+                    );
+                }
+            }
+        }
+    }
+
+    /**
+     * <p>Updates all actor objects within the current level.</p>
+     * @param delta The ratio of actual/target update rate for the game ticks.
+     */
+    public void updateActors(float delta) {
+
+        // Update all Actors
+        for (AActor gameObject : actors) {
+
+            // Update TestActors
+            if (gameObject instanceof Particle a) {
+                a.update(delta);
+            }
+
+            // Update Characters
+            if (gameObject instanceof PlayerAvatar tc) {
+                tc.control(delta);
+                tc.update(delta);
+                //System.out.println(tc.actionState);
+            }
+        }
+    }
+
+    /**
+     * <p>Detects the collisions of platforms with actors. Resolves the collisions within the actor and platform
+     * objects themselves.</p>
+     * @param delta The ratio of actual/target update rate for the game ticks.
+     */
+    private void detectCollisions(float delta) {
+        for (AProp p : levelsList.getCurrentLevel().getLevelProps()) {
+            for (AActor a : actors) {
+                p.hasCollision(a, delta);
+            }
+        }
+    }
+
+    /**
+     * <p>Inserts an actor from the queue into the primary list of actors.</p>
+     */
+    private void insertQueuedActors() {
+        for(int i = 0; i < 10 && actorsQueue.size() >= 1; i++) {
+            addActor(actorsQueue.remove());
+        }
+    }
+
+    /**
+     * <p>Queues an actor into the queue list.
+     * Actors are incrementally shifted to the main list when process is available.</p>
+     * @param a Queues an actor to be added to the main list.
+     */
+    public void queueActor(AActor a) {
+        actorsQueue.add(a);
+    }
+
+    /**
+     * <p>Adds an actor to the main actors list.</p>
+     * @param actor The actor to be added to the list
+     */
+    public void addActor(AActor actor) {
+        actors.add(actor);
+    }
+
+    /**
+     * <p>Gets the current level that's active.</p>
+     * @return the current level object
+     */
+    public ALevel getCurrentLevel() {
+        return levelsList.getCurrentLevel();
+    }
+
+    /**
+     * <p>Gets the PlayerAvatar object.</p>
+     * @return the player
+     */
+    public ACharacter getPlayerAvatar() {
+        return character;
+    }
+
+    /**
+     * <p>Gets the player inventory. Inventory contains references to collectibles, etc.</p>
+     * @return the player's inventory.
+     */
+    public PlayerInventory getPlayerInventory() {
+        return inventory;
     }
 
     @Override
@@ -143,248 +387,6 @@ public class GameEnvironment extends AEnvironment implements IDrawable, IUpdatab
             hudModel.draw(g2d);
         }
 
-    }
-
-    /**
-     * <p></p>
-     * @param delta -
-     */
-    public void doGameUpdates(float delta) {
-
-        doGameControls();
-        insertQueuedActors(); // Dequeue queued actors and add them to list of actors
-        detectCollisions(delta); // Check Game Object Collisions with Level Props
-        updateActors(delta); // Update the Game Objects
-        updateLevel(delta); // Update level models.props
-        updateHUD(delta); // Update HUD overlay
-    }
-
-    /**
-     * <p></p>
-     * @param delta -
-     */
-    public void doPauseMenuUpdates(float delta) {
-        doPauseMenuControls();
-
-        pauseMenuEnvironment.update(delta);
-    }
-
-    /**
-     * <p></p>
-     */
-    private void doGameControls() {
-        robot.mouseMove(
-                (int) (Toolkit.getDefaultToolkit().getScreenSize().width * .5f),
-                (int) (Toolkit.getDefaultToolkit().getScreenSize().height * .5f)
-        );
-
-        if(getKeyController() instanceof GameKeyControls kc) {
-            if(kc.getControlsModel().getAction(GameControls.Actions.ESCAPE)) {
-                kc.getControlsModel().reset();
-                setPaused(true);
-                getParentEnvironmentsHandler().swapToEnvironment(
-                        EnvironmentsHandler.EnvironmentType.GAME_PAUSE_MENU, false).applyEnvironment();
-            }
-        }
-    }
-
-    /**
-     * <p></p>
-     */
-    public void doPauseMenuControls() {
-        if(pauseMenuEnvironment.getKeyController() instanceof MenuKeyControls kc) {
-            if(kc.getControlsModel().getAction(MenuControls.Actions.ESCAPE)) {
-                kc.getControlsModel().reset();
-                setPaused(false);
-                pauseMenuEnvironment.onExit();
-                getParentEnvironmentsHandler().swapToEnvironment(
-                        EnvironmentsHandler.EnvironmentType.GAME, false).applyEnvironment();
-            }
-        }
-    }
-
-    /**
-     * <p></p>
-     * @param levelsList -
-     */
-    public void setLevelsList(LevelsList levelsList) {
-        this.levelsList = levelsList;
-    }
-
-    /**
-     * <p></p>
-     * @return
-     */
-    public LevelsList getLevelsList() {
-        return levelsList;
-    }
-
-    /**
-     * <p></p>
-     * @param controlsViewModel -
-     * @param levelModel -
-     */
-
-    private void setPlayerAvatar(GameControls controlsViewModel, LevelsList levelModel) {
-        int[] startPos = levelModel.getCurrentLevel().getCharacterOrigin();
-        // Add in the Main Test Character
-
-        character = new PlayerAvatar (
-                getResources(),
-                controlsViewModel,
-                startPos[0],
-                startPos[1],
-                55, 70,
-                0, 0,
-                true
-        );
-
-    }
-
-    /**
-     * <p></p>
-     * @param delta -
-     */
-    private void updateHUD(float delta) {
-        hudModel.update(delta);
-    }
-
-    /**
-     * <p></p>
-     * @param delta -
-     */
-    private void updateLevel(float delta) {
-        levelsList.getCurrentLevel().update(delta);
-    }
-
-    /**
-     * <p></p>
-     * @param delta -
-     */
-    private synchronized void testAddingActors(float delta) {
-        if (getMouseController() instanceof GameMouseControls gmc) {
-
-            if (gmc.isLeftPressed()) {
-                int count = (int) (10 / delta);
-                if (count < 1) {
-                    count = 1;
-                }
-                for (int i = 0; i < count; i++) {
-                    queueActor(
-                            new Particle(
-                                    getResources(),
-                                    (-Camera.camX / Config.scaledW_zoom) + (gmc.getPos()[0]/ Config.scaledW_zoom),
-                                    (-Camera.camY / Config.scaledW_zoom) + (gmc.getPos()[1]/ Config.scaledH_zoom),
-                                    10f,
-                                    10f,
-                                    new Random().nextFloat(-5, 5),
-                                    new Random().nextFloat(-5, 5),
-                                    true
-                            )
-                    );
-                }
-            }
-        }
-    }
-
-    /**
-     * <p></p>
-     * @param delta -
-     */
-    public void updateActors(float delta) {
-
-        // Update all Actors
-        for (AActor gameObject : actors) {
-
-            // Update TestActors
-            if (gameObject instanceof Particle a) {
-                a.update(delta);
-            }
-
-            // Update Characters
-            if (gameObject instanceof PlayerAvatar tc) {
-                tc.control(delta);
-                tc.update(delta);
-                //System.out.println(tc.actionState);
-            }
-        }
-    }
-
-    /**
-     * <p></p>
-     * @param delta -
-     */
-    private void detectCollisions(float delta) {
-        for (AProp p : levelsList.getCurrentLevel().getLevelProps()) {
-            for (AActor a : actors) {
-                p.hasCollision(a, delta);
-            }
-        }
-    }
-
-    /**
-     * <p></p>
-     */
-    private void insertQueuedActors() {
-        for(int i = 0; i < 10 && actorsQueue.size() >= 1; i++) {
-            addActor(actorsQueue.remove());
-        }
-    }
-
-    /**
-     * <p></p>
-     * @param a -
-     */
-    public void queueActor(AActor a) {
-        actorsQueue.add(a);
-    }
-
-    /**
-     * <p></p>
-     * @param actor -
-     */
-    public void addActor(AActor actor) {
-        actors.add(actor);
-    }
-
-    /**
-     * <p></p>
-     * @param levelIndex -
-     */
-    public void setCurrentLevel(int levelIndex) {
-        levelsList.setCurrentLevel(levelIndex);
-    }
-
-    /**
-     * <p></p>
-     * @return
-     */
-    public ALevel getCurrentLevel() {
-        return levelsList.getCurrentLevel();
-    }
-
-    /**
-     * <p></p>
-     * @return
-     */
-    public ACharacter getPlayerAvatar() {
-        return character;
-    }
-
-    /**
-     * <p></p>
-     * @return
-     */
-    public PlayerInventory getPlayerInventory() {
-        return inventory;
-    }
-
-    /**
-     * <p></p>
-     * @param paused -
-     */
-    public void setPaused(boolean paused) {
-        isPaused = paused;
     }
 
     @Override
